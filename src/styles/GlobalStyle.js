b0243f4051c284a254a6bff1379f6a3c3241316f
timcho19 <timcho4589@gmail.com>
2025-08-29 12:08:58 +0900

    ⚙️react 초기 환경 셋팅

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Noto Sans KR', sans-serif;
+    background: #f8f8f8;
+  }
+`;
+
+export default GlobalStyle;

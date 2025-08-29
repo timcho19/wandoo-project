cfcd02bb8d6c569db82ca1b8c24f62f76a661f0f
Ha-im <gungsun1@naver.com>
2025-08-29 16:11:20 +0900

    📝 Signup,Login 페이지 추가 및 jsx문법 수정

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import GlobalStyle from './styles/GlobalStyle';
 import Home from './pages/Home';
 import Mypage from './pages/Mypage';
 import Bubble from './components/Bubble';
+import Login from './pages/login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           {/* 예시: <Route path="/login" element={<LoginPage />} /> */}
           <Route path="/" element={<Home/>}/> 
           <Route path="/mypage" element={<Mypage/>}/> 
+          <Route path="/login" element={<Login/>}/> 
+          <Route path="/signup" element={<Signup/>}/> 
         </Routes>
       </BrowserRouter>
       <Bubble/>

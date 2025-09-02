2824f0fdd215b0ccf66387d60f10b9cd8f9a09f0
Ha-im <gungsun1@naver.com>
2025-09-02 15:10:44 +0900

    🛠 네브컴포넌트 수정중 로그인 시 마이페이지 표시

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,27 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { supabase } from "../supabase";
 
 export default function Nav() {
+    const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // 초기 로드 시 로그인 상태 확인
+    const session = supabase.auth.getSession().then(({ data }) => {
+      setUser(data.session?.user || null);
+    });
+
+    // Auth 상태 변경 감지 (로그인/로그아웃)
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user || null);
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+
   return (
     <nav className="bottom-nav">
       <div className="bottom-nav-bg"></div>
@@ -17,10 +38,11 @@ export default function Nav() {
           <img src="/image/icon/category.svg" alt="모임찾기"/>
           <span className="nav-label-inactive">모임찾기</span>
         </Link>
-        <Link to="/mypage" className="nav-item">
-          <img src="/image/icon/mypage.svg" alt="마이페이지"/>
-          <span className="nav-label-inactive">마이페이지</span>
-        </Link>
+        {user ? (
+          <Link to="/mypage">마이페이지</Link>
+        ) : (
+          <Link to="/login">로그인</Link>
+        )}
       </div>
     </nav>
   );
